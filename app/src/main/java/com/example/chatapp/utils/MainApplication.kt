018c0c951721fdd3b7af98e84d32ee53ad7d237f
Hernani Steinheuser <hernanistn@gmail.com>
2021-07-27 13:04:03 -0300

    first commit 27/07

--- app/src/main/java/com/example/chatapp/utils/MainApplication.kt
+++ app/src/main/java/com/example/chatapp/utils/MainApplication.kt
@@ -2,15 +2,25 @@ package com.example.chatapp.utils
 
 import android.app.Application
 import android.content.Context
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class MainApplication : Application(){
+
     init {
     instance = this
+    applicationScope = CoroutineScope(SupervisorJob())
     }
     companion object{
         private lateinit var instance : Context
+        private lateinit var applicationScope: CoroutineScope
         fun getContextInstance(): Context {
             return instance.applicationContext
         }
+        fun getCoroutineScope(): CoroutineScope{
+            return applicationScope
+        }
+
+
     }
 }
