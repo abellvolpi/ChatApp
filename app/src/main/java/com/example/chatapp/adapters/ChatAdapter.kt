261e40991ba69f3db43386e325bcdd1652a53eba
Hernani Steinheuser <hernanistn@gmail.com>
2021-07-29 10:17:01 -0300

    FUNCIONANDO COM AUXILIO DO INDIANO

--- app/src/main/java/com/example/chatapp/adapters/ChatAdapter.kt
+++ app/src/main/java/com/example/chatapp/adapters/ChatAdapter.kt
@@ -1,47 +1,84 @@
 package com.example.chatapp.adapters
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.chatapp.R
+import com.example.chatapp.databinding.MessageNotifyItemBinding
 import com.example.chatapp.databinding.MessageReceivedItemBinding
-import com.example.chatapp.utils.Utils
+import com.example.chatapp.databinding.MessageSentItemBinding
 import com.example.chatapp.models.Message
 import java.text.SimpleDateFormat
 
 class ChatAdapter(var data: ArrayList<Message>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private lateinit var binding: MessageReceivedItemBinding
+    private lateinit var bindingSent: MessageSentItemBinding
+    private lateinit var bindingReceived: MessageReceivedItemBinding
+    private lateinit var bindingNotify: MessageNotifyItemBinding
 
-    inner class ViewHolder(binding: MessageReceivedItemBinding) :
+    inner class ViewHolderReceiveMessage(binding: MessageReceivedItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding =
-            MessageReceivedItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    inner class ViewHolderSentMessage(binding: MessageSentItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    inner class ViewHolderNotifyMessage(binding: MessageNotifyItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            Message.SENT_MESSAGE -> {
+                bindingSent = MessageSentItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderSentMessage(bindingSent)
+            }
+
+            Message.RECEIVED_MESSAGE -> {
+                bindingReceived = MessageReceivedItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderReceiveMessage(bindingReceived)
+            }
+            Message.NOTIFY_CHAT -> {
+                bindingNotify = MessageNotifyItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                return ViewHolderNotifyMessage(bindingNotify)
+            }
+            else -> return ViewHolderNotifyMessage( //apenas para validar um else
+                MessageNotifyItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return position
+        return data[position].typeMesage
     }
 
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val context = holder.itemView.context
-        when (data[position].typeMesage) {
+        when (getItemViewType(position)) {
             Message.SENT_MESSAGE -> {
-                setSentMessageParams(context, data[position])
+                bindingSent.message.text = data[position].message
+                bindingSent.name.text = data[position].name
+                bindingSent.time.text = timeFormatter(data[position].date)
             }
             Message.RECEIVED_MESSAGE -> {
-                setReceivedMessageParams(context, data[position])
+                bindingReceived.message.text = data[position].message
+                bindingReceived.name.text = data[position].name
+                bindingReceived.time.text = timeFormatter(data[position].date)
             }
             Message.NOTIFY_CHAT -> {
-                setNotificationLayoutParams(context, data[position])
+                bindingNotify.message.text = data[position].message
             }
         }
     }
@@ -54,63 +91,13 @@ class ChatAdapter(var data: ArrayList<Message>) : RecyclerView.Adapter<RecyclerV
         return position.toLong()
     }
 
-    @SuppressLint("UseCompatLoadingForDrawables")
-    private fun setNotificationLayoutParams(context: Context, messageF: Message) {
-        with(binding) {
-            messageF.let {
-                mainLinearLayoutChat.gravity = Gravity.CENTER
-                childLinearLayoutChat.background = context.getDrawable(R.drawable.notify_shape)
-                name.textAlignment = View.TEXT_ALIGNMENT_CENTER
-                message.text = it.message
-                name.visibility = View.GONE
-                time.visibility = View.GONE
-                name.text = ""
-                time.text = ""
-            }
-        }
-    }
-
-    @SuppressLint("UseCompatLoadingForDrawables")
-    private fun setReceivedMessageParams(context: Context, messageF: Message) {
-        with(binding) {
-            messageF.let {
-                mainLinearLayoutChat.gravity = Gravity.START
-                childLinearLayoutChat.background =
-                    context.getDrawable(R.drawable.received_message_shape)
-                time.text = timeFormatter(it.date)
-                name.text = it.name
-                name.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
-                message.text = it.message
-                name.visibility = View.VISIBLE
-                time.visibility = View.VISIBLE
-            }
-        }
-    }
-
-    @SuppressLint("UseCompatLoadingForDrawables")
-    private fun setSentMessageParams(context: Context, messageF: Message) {
-        with(binding) {
-            messageF.let {
-                mainLinearLayoutChat.gravity = Gravity.END
-                childLinearLayoutChat.background =
-                    context.getDrawable(R.drawable.sent_message_shape)
-                time.text = timeFormatter(it.date)
-                name.text = "You"
-                name.textAlignment = View.TEXT_ALIGNMENT_TEXT_END
-                message.text = it.message
-                name.visibility = View.VISIBLE
-                time.visibility = View.VISIBLE
-            }
-        }
-    }
-
     @SuppressLint("SimpleDateFormat")
-    private fun timeFormatter(time: Long): String{
+    private fun timeFormatter(time: Long): String {
         val dtf = SimpleDateFormat("HH:mm")
         return dtf.format(time)
     }
 
-    fun addData(message: Message){
+    fun addData(message: Message) {
         data.add(message)
     }
 }
--- app/src/main/java/com/example/chatapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/HomeFragment.kt
@@ -45,7 +45,6 @@ class HomeFragment : Fragment() {
     }
 
     private fun initViews() {
-        Utils.createNotification("Teste", "mensagem teste hjbdshkahsdakshd")
         with(binding) {
             connect.setOnClickListener {
                 if (!isEditTextIsEmpty()) {
