b0ff1357f54cad31aeb78a787a4cd308f801ab51
Hernani Steinheuser <hernanistn@gmail.com>
2021-07-30 16:59:21 -0300

    bottomsheet funcionando meia boca

--- app/src/main/java/com/example/chatapp/ui/ChatFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/ChatFragment.kt
@@ -3,13 +3,13 @@ package com.example.chatapp.ui
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,19 +19,19 @@ import com.example.chatapp.databinding.FragmentChatBinding
 import com.example.chatapp.models.Board
 import com.example.chatapp.models.Cell
 import com.example.chatapp.models.Message
-import com.example.chatapp.viewModel.ConnectionFactory
 import com.example.chatapp.utils.ProfileSharedProfile
 import com.example.chatapp.utils.Utils
 import com.example.chatapp.utils.Utils.hideSoftKeyboard
+import com.example.chatapp.viewModel.ConnectionFactory
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.snackbar.Snackbar
 
 class ChatFragment : Fragment() {
-    private lateinit var binding : FragmentChatBinding
-    private val connectionFactory : ConnectionFactory by activityViewModels()
+    private lateinit var binding: FragmentChatBinding
+    private val connectionFactory: ConnectionFactory by activityViewModels()
     private lateinit var adapter: ChatAdapter
-    private var bottomsheetForConfig = BottomSheetBehavior.from(requireView().findViewById(R.id.bottom_sheet))
     private var data = arrayListOf<Message>()
+    private lateinit var bottomsheetForConfig: BottomSheetBehavior<View>
     private lateinit var profileName: String
     private val navController by lazy {
         findNavController()
@@ -41,8 +41,9 @@ class ChatFragment : Fragment() {
     //bottomsheet
     private val boardCells = Array(3) { arrayOfNulls<ImageButton>(3) } // Array de image button
     private var board = Board()
-    private var canIPlay : Boolean = false
+    private var canIPlay: Boolean = false
     private var player = ""
+    private var isTicTacToePlayRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,8 +67,20 @@ class ChatFragment : Fragment() {
             activity?.hideSoftKeyboard()
         }
         binding.tictactoe.setOnClickListener {
-            sendInviteTicTacToe()
+            if (!isTicTacToePlayRunning) {
+                sendInviteTicTacToe()
+            } else {
+                Snackbar.make(
+                    requireView(),
+                    "The game has been started. To return, swipe up",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+
         }
+        bottomsheetForConfig =
+            BottomSheetBehavior.from(requireView().findViewById(R.id.bottom_sheet))
+        bottomsheetForConfig.peekHeight = 150
     }
 
     private fun initView() {
@@ -115,13 +128,13 @@ class ChatFragment : Fragment() {
 
     private fun refreshUIChat(message: Message) {
         adapter.addData(message)
-        if(message.typeMesage == Message.SENT_MESSAGE){
-            binding.messagesRecyclerview.scrollToPosition(data.size-1)
+        if (message.typeMesage == Message.SENT_MESSAGE) {
+            binding.messagesRecyclerview.scrollToPosition(data.size - 1)
             return
         }
-        binding.messagesRecyclerview.apply{
-            if(!canScrollVertically(1)){
-                scrollToPosition(data.size-1)
+        binding.messagesRecyclerview.apply {
+            if (!canScrollVertically(1)) {
+                scrollToPosition(data.size - 1)
             }
         }
     }
@@ -137,9 +150,12 @@ class ChatFragment : Fragment() {
             mapBoardToUi()
             binding.bottomSheet.whoPlay.text = "Sua vez de jogar. Você é ${player}"
             canIPlay = true
+            verifyIfHasWinner()
+            return
         }
         if (message.typeMesage == Message.INVITE_TICTACTOE) {
-            if(message.message == "accepted"){
+            if (message.message == "accepted") {
+                refreshBoard()
                 snackbar.dismiss()
                 player = Board.O
                 canIPlay = true
@@ -147,9 +163,13 @@ class ChatFragment : Fragment() {
                 bottomsheetForConfig.state = BottomSheetBehavior.STATE_EXPANDED
                 return
             }
-            if(message.message == "declined"){
+            if (message.message == "declined") {
                 snackbar.dismiss()
-                snackbar = Snackbar.make(requireView(), "Jogador recusou seu convite", Snackbar.LENGTH_LONG)
+                snackbar = Snackbar.make(
+                    requireView(),
+                    "Jogador recusou seu convite",
+                    Snackbar.LENGTH_LONG
+                )
                 snackbar.show()
                 return
             }
@@ -158,6 +178,8 @@ class ChatFragment : Fragment() {
         }
         if (message.typeMesage != Message.NOTIFY_CHAT) {
             message.typeMesage = Message.RECEIVED_MESSAGE
+            refreshUIChat(message)
+        }else{
             refreshUIChat(message)
         }
     }
@@ -176,34 +198,39 @@ class ChatFragment : Fragment() {
         builder.show()
     }
 
-    private fun sendInviteTicTacToe(){
+    private fun sendInviteTicTacToe() {
         ProfileSharedProfile.getProfile {
             val message = Message(it, "", Message.INVITE_TICTACTOE)
-            connectionFactory.sendMessage(message){
-                snackbar = Snackbar.make(requireView(), "Aguardando oponente aceitar a partida", Snackbar.LENGTH_LONG)
+            connectionFactory.sendMessage(message) {
+                snackbar = Snackbar.make(
+                    requireView(),
+                    "Aguardando oponente aceitar a partida",
+                    Snackbar.LENGTH_LONG
+                )
                 snackbar.show()
             }
         }
-
     }
 
-    private fun acceptInviteTicTacToe(){
+    private fun acceptInviteTicTacToe() {
+        refreshBoard()
         player = Board.X
         canIPlay = false
         initViewTicTacToe()
         bottomsheetForConfig.state = BottomSheetBehavior.STATE_EXPANDED
         val message = Message("", "accepted", Message.INVITE_TICTACTOE)
-        connectionFactory.sendMessage(message){}
+        connectionFactory.sendMessage(message) {}
     }
 
-    private fun declineInviteTicTacToe(){
+    private fun declineInviteTicTacToe() {
         val message = Message("", "declined", Message.INVITE_TICTACTOE)
-        connectionFactory.sendMessage(message){}
+        connectionFactory.sendMessage(message) {}
     }
 
     //bottom sheet functions
     private fun initViewTicTacToe() {
-        with(binding.bottomSheet){
+        isTicTacToePlayRunning = true
+        with(binding.bottomSheet) {
             boardCells[0][0] = btn1
             boardCells[0][1] = btn2
             boardCells[0][2] = btn3
@@ -217,6 +244,9 @@ class ChatFragment : Fragment() {
                 whoPlay.text = "Sua vez de jogar. Você é ${player}!"
             } else {
                 whoPlay.text = "Esperando o oponente jogar..."
+            }
+            rematchButton.setOnClickListener {
+                sendInviteTicTacToe()
             }
         }
         callClickListener()
@@ -236,6 +266,7 @@ class ChatFragment : Fragment() {
                 val cell = Cell(i, j)
                 board.placeMove(cell, player)
                 sendPlay(i, j)
+                verifyIfHasWinner()
             }
             mapBoardToUi()
         }
@@ -269,6 +300,45 @@ class ChatFragment : Fragment() {
             binding.bottomSheet.whoPlay.text = "Esperando o oponente jogar..."
             canIPlay = false
             Log.e("Sent", "play")
+            verifyIfHasWinner()
+        }
+    }
+
+    private fun refreshBoard() {
+        board = Board()
+        mapBoardToUi()
+    }
+
+    private fun verifyIfHasWinner() {
+        with(binding.bottomSheet) {
+            if (board.gameOver()) {
+                if (board.playerWon()) { //player O
+                    whoPlay.text = "Player O wins!"
+                    rematchButton.visibility = View.VISIBLE
+                    if (player == Board.O) {
+                        ProfileSharedProfile.getProfile {
+                            val message =
+                                Message("", "${it} winner TicTacToe!", Message.NOTIFY_CHAT)
+                            connectionFactory.sendMessage(message) {}
+                        }
+                    }
+                    return
+                }
+                if (board.opponentWon()) {
+                    whoPlay.text = "Player X wins!"
+                    rematchButton.visibility = View.VISIBLE
+                    if (player == Board.X) {
+                        ProfileSharedProfile.getProfile {
+                            val message = Message("", "${it} winner TicTacToe!", Message.NOTIFY_CHAT)
+                            connectionFactory.sendMessage(message) {}
+                        }
+                    }
+                    return
+                }
+                whoPlay.text = "The game is DRAW!"
+                rematchButton.visibility = View.VISIBLE
+                return
+            }
         }
     }
 }
