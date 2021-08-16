44381f508117116a9887851fe59b228b63a26439
Hernani Steinheuser <hernanistn@gmail.com>
2021-08-16 10:38:43 -0300

    commit with checkout tictactoe 2

--- app/src/main/java/com/example/chatapp/room/appDataBase/AppDataBase.kt
+++ app/src/main/java/com/example/chatapp/room/appDataBase/AppDataBase.kt
@@ -3,9 +3,9 @@ package com.example.chatapp.room.AppDataBase
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.chatapp.models.Profile
-import com.example.chatapp.room.Profile.DAO.ProfileDAO
+import com.example.chatapp.room.profile.dao.ProfileDAO
 
 @Database(version = 1, entities = [Profile::class], exportSchema = false)
 abstract class AppDataBase: RoomDatabase() {
-    abstract fun profileDAO():ProfileDAO
+    abstract fun profileDAO(): ProfileDAO
 }
